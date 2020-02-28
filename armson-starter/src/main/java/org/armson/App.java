429a62187f1bc0693d54428e26da882f2ac37763
mzhcg123 <mzhcg123@163.com>
2020-02-28 20:01:43 +0800

    删除无用的Testt文件

---
+++ armson-starter/src/main/java/org/armson/App.java
@@ -0,0 +1,13 @@
+package org.armson;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
